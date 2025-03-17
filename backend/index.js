125115121a41822763c9b4e2de5c8fa28c8cdf55
Priyanshu Gupta <realpriyanshugupta@gmail.com>
2025-03-17 23:23:53 +0530

    vercel frontend deploy

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,5 @@ app.use(express.json())
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/account", accountRouter)
 
-app.listen(3000, ()=>console.log("listening at 3000!"))
 
 export default app
