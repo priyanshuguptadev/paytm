127ef1ecf8be58f62426a7bed859304ac3f10491
Priyanshu Gupta <realpriyanshugupta@gmail.com>
2025-03-18 00:25:15 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,12 @@ import accountRouter from './routes/account.js'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  origin: 'https://paytm-frontend-three.vercel.app/'
+}))
 app.use(express.json())
 
 app.use("/api/v1/user", userRouter)
