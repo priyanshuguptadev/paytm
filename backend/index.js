d19610d4bd5095f63276ad65d33d98e635751f8b
Priyanshu Gupta <realpriyanshugupta@gmail.com>
2025-03-19 10:32:39 +0530

    add hashing

--- backend/index.js
+++ backend/index.js
@@ -13,4 +13,5 @@ app.use(express.json())
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/account", accountRouter)
 
+// app.listen(3000, ()=>console.log('listening at port 3000'))
 export default app
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,11 +2,11 @@ import express from 'express'
 import { userSignInSchema,updateSchema, userSignUpSchema } from '../schema/index.js' 
 import { User, Account } from '../db.js'
 import * as jose from 'jose'
+import bcrypt from 'bcryptjs'
 import { authMiddleware } from '../middleware.js'
 
 const userRouter = express.Router()
 
-// Create a secret key for JWT signing
 const secretKey = new TextEncoder().encode(process.env.JWT_SECRET)
 
 userRouter.post("/signup", async (req, res) => {
@@ -16,16 +16,23 @@ userRouter.post("/signup", async (req, res) => {
     res.json({msg: 'wrong inputs'})
     return;
   }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(body.password, salt);
   try {
     const findUser = await User.findOne({
       username: body.username,
-      password: body.password
+      password: hashedPassword
     })
     if(findUser){
       res.send({msg: 'user already exists! please sign in'})
       return;
     }
-    const user = new User(body)
+    const user = new User({
+      firstName: body.firstName,
+      lastName: body.lastName,
+      username: body.username,
+      password: hashedPassword
+    })
     const status = await user.save()
     const accountCreated = await Account.create({
       accountId: status.id,
@@ -35,7 +42,6 @@ userRouter.post("/signup", async (req, res) => {
       return res.status(403).json({msg: 'account creation failed'})
     }
     
-    // Create JWT using jose
     const token = await new jose.SignJWT({ id: status.id })
       .setProtectedHeader({ alg: 'HS256' })
       .setExpirationTime('24h')
@@ -56,12 +62,12 @@ userRouter.post("/signin", async (req, res) => {
     return;
   }
   try {
-    const user = await User.findOne(body)
-    if (!user) {
+    const user = await User.findOne({username: body.username})
+    const checkPassword = await bcrypt.compare(body.password, user.password);
+    if (!checkPassword) {
       return res.status(401).json({ err: 'Invalid credentials' })
     }
     
-    // Create JWT using jose
     const token = await new jose.SignJWT({ id: user.id })
       .setProtectedHeader({ alg: 'HS256' })
       .setExpirationTime('24h')
